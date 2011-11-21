0cefaf9fbdad57e436a5c575d02fe7d65b84dbc7
Ben Wiseley <wiseleyb@gmail.com>
2011-11-21 18:58:52 +1100

    ruff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bow::Application.config.secret_token = 'f21c4109a8a25bf4832ae61367429ec039712194ee4d347ebd01933b242783c637e9789997f19353c6c39c354f4577e3b5f6f45ce5bbf6e077c26bcfe293249d'
